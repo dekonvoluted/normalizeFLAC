897c321a86d44b81c32dd2d3c0ba5e4c186197f8
Karthik Periagaram <karthik.periagaram@gmail.com>
2014-07-04 13:47:44 -0700

    Initial commit

---
+++ normalizeFLAC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Apply replay gain normalization to FLAC files
+# Files are found recursively in the current directory
+# A different directory than the current directory may be specified
+# A single FLAC file may also be specified
+
+exit 0
+
